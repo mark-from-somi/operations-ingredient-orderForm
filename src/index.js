cc311c022802393690394b7306c1a832d9ba673d
Mark <79877319+somiMission@users.noreply.github.com>
2022-11-02 18:01:56 -0700

    added html
    form repeater

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// Code goes here
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+function doGet() {
+  return HtmlService.createTemplateFromFile('page').evaluate();
+}
+
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
